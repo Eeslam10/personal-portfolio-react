1866a7600fd75ddbe0f2975e628b84ca74201901
islam <islammuhd8@gmail.com>
2024-07-31 17:48:43 -0700

    updates

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
     <div id='home' className='hero'>
         <img src={profile_img} alt="" />
         <h1> <span>Hassan Lawal,</span> frontend developer based in Nigeria.</h1>
-        <p>I'm a frontend developer from Abuja, Nigeria with 4years experience in Katsina Tech Hub and ICT Hassan Usman Katsina Polytechnic</p>
+        <p>I'm a frontend developer from Katsina, Nigeria with 3years experience in Katsina Tech Hub and ICT Hassan Usman Katsina Polytechnic</p>
         <div className='hero-action'>
             <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink> </div>
             <div className="hero-resume">My resume</div>
